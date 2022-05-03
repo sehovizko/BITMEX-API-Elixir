8e76c1b75cc35a56657733aafbbee9c2630fac28
Zlatko <95984933+sehovizko@users.noreply.github.com>
2022-05-03 09:07:04 +0200

    Bump ex_doc & excoveralls

--- test/ex_bitmex/rest/user/wallet_history_test.exs
+++ test/ex_bitmex/rest/user/wallet_history_test.exs
@@ -1,9 +1,9 @@
-defmodule ExBitmex.Rest.User.CommissionTest do
+defmodule ExBitmex.Rest.User.WalletHistoryTest do
   use ExUnit.Case, async: false
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
   import Mock
   alias ExBitmex.Rest.User
-  doctest ExBitmex.Rest.User.Commission
+  doctest ExBitmex.Rest.User.WalletHistory
 
   setup_all do
     HTTPoison.start()
@@ -15,16 +15,15 @@ defmodule ExBitmex.Rest.User.CommissionTest do
     api_secret: System.get_env("BITMEX_API_SECRET")
   }
 
-  test ".get account commission status" do
-    use_cassette "rest/user/commission_ok" do
-      assert {:ok, commission, _} = User.Commission.get(@credentials)
-      assert Enum.any?(commission) == true
+  test ".get returns the wallet history information" do
+    use_cassette "rest/user/wallet_history_ok" do
+      assert {:ok, [%ExBitmex.WalletHistory{currency: "XBt"} | _tail], _} = User.WalletHistory.get(@credentials, %{symbol: "XBt", count: 10})
     end
   end
 
   test ".get bubbles errors" do
     with_mock HTTPoison, request: fn _url -> {:error, %HTTPoison.Error{reason: :timeout}} end do
-      assert {:error, :timeout, nil} = User.Commission.get(@credentials)
+      assert {:error, :timeout, nil} = User.WalletHistory.get(@credentials)
     end
   end
 end
