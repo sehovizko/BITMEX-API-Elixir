5e25e0d5e575b4a9adb492fd9b4815409a410fa7
Zlatko <95984933+sehovizko@users.noreply.github.com>
2022-05-06 12:42:49 +0200

    Reorder mix deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBitmex.Mixfile do
   def project do
     [
       app: :ex_bitmex,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.7",
       package: package(),
       start_permanent: Mix.env() == :prod,
@@ -31,14 +31,14 @@ defmodule ExBitmex.Mixfile do
       {:httpoison, "~> 1.0"},
       {:jason, "~> 1.1"},
       {:mapail, "~> 1.0"},
+      {:websockex, "~> 0.4"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:mock, "~> 0.3", only: :test},
       {:exvcr, "~> 0.10", only: [:dev, :test]},
       {:ex_unit_notifier, "~> 0.1", only: :test},
-      {:excoveralls, "~> 0.10", only: :test},
-      {:websockex, "~> 0.4"}
+      {:excoveralls, "~> 0.1", only: :test}
     ]
   end
 
